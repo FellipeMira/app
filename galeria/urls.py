c195a71d1f202f51f90235b1b6ea8f63d9c1ca92
FellipeMira <fellipe.mira@unesp.br>
2023-04-03 22:27:19 -0300

    part 2 -  Admin db

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from galeria.views import index, map 
 urlpatterns = [
-    path('', index),
-    path('map/',map)
+    path('', index, name='index'),
+    path('map/', map, name='map')
 ]
